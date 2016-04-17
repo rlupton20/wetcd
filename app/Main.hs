4e89149a7ae37b1e3d38c9cc0394ad75cb4019e8
Richard <richard.lupton@gmail.com>
2016-04-17 14:34:19 +0100

    Basic warp server running. Doesn't do anything intelligent.

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Config
+
+import Network.Wai
+import Network.HTTP.Types
+import Network.Wai.Handler.Warp
 
 main :: IO ()
 main = do
        conf <- loadConfigFromFile "testConf.yaml"
        putStrLn $ show conf
+       run 8000 testApp
+       
+
+testApp :: Application
+testApp req respond = respond $ responseLBS status200 [] "Testing!"
